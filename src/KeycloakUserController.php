e9edb74a5e76992e881cb212a312763659f0e086
vredko-freysoft <127108123+vredko-freysoft@users.noreply.github.com>
2023-06-07 20:24:06 +0300

    Update KeycloakUserController.php

--- src/KeycloakUserController.php
+++ src/KeycloakUserController.php
@@ -2,20 +2,12 @@
 
 namespace KeycloakGuard;
 
-use App\Models\User;
-use Illuminate\Contracts\Auth\Guard;
-use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\UserProvider;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
-use KeycloakGuard\Exceptions\TokenException;
-use KeycloakGuard\Exceptions\UserNotFoundException;
-use KeycloakGuard\Exceptions\ResourceAccessNotAllowedException;
-
 use App\Common\AppConst;
-use App\Logger\Facades\Logger;
-use App\Logger\Facades\AppLog;
 use App\Http\Controllers\KeycloakAdminController;
+use App\Logger\Facades\AppLog;
+use App\Logger\Facades\Logger;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class KeycloakUserController
 {
@@ -29,79 +21,40 @@ class KeycloakUserController
     }
 
 
-
     /**
      * Create new User & store into users DB of App
      *
-     * @param $user
-     * @return $user
-    */
-    public function createUser($user)
+     * @param $class
+     * @return User $user
+     */
+    public function createUser($class)
     {
+        $user = [];
         // email & verified state
-        $user->email = $this->decodedToken->email;
-        $user->email_verified_at = date('Y-m-d H:i:s');
+        $user['email'] = $this->decodedToken->email;
+        $user['email_verified_at'] = date('Y-m-d H:i:s');
 
         $name = !empty($this->decodedToken->name)
             ? $this->decodedToken->name
             : str_replace('@', ' ', $this->decodedToken->email);
 
         // name
-        $user->name = $this->decodedToken->given_name
-            ?:substr($name,0,strpos($name,' '));
+        $user['name'] = $this->decodedToken->given_name
+            ?: substr($name, 0, strpos($name, ' '));
 
-        $user->surname = $this->decodedToken->family_name
-            ?:substr($name,strpos($name,' ')+1);
-
-        // locales
-        $user->locale = 'en';
-        $user->locale_templates = 'en';
-
-        // wait a little before check in -- 1-3 seconds
-        // cause a couple of concurrency requests can come in same time
-        //sleep(2);
-
-        // check if concurrency queries already saved a new User
-        // return before saving
-        if (DB::table(AppConst::USERS_TABLE_NAME)->where('email', $user->email)->exists()) {
-            return $user;
-        }
+        $user['surname'] = $this->decodedToken->family_name
+            ?: substr($name, strpos($name, ' ') + 1);
 
         // pass
-        $newPassword = substr($this->decodedToken->sid,0,6);
-        $user->password = bcrypt($newPassword);
+        $newPassword = substr($this->decodedToken->sid, 0, 6);
+        $user['password'] = bcrypt($newPassword);
+        $user['rainex_id'] = \Str::uuid();
 
         // create User if none
-        try {
-            $user = User::updateOrCreate(
-                [
-                    'email' => $user->email
-                ],
-                [
-                    'updated_at' => date('Y-m-d H:i:s'),
-                ]
-            );
+        DB::table(AppConst::USERS_TABLE_NAME)->insert($user);
 
-            //$user->save();
-
-            //
-            $user_id = (int)$user->getKey();
-
-            // app log User
-            AppLog::add('user', $user_id, 'create');
-        }
-        // something went wrong while
-        catch (\Throwable $e) {
-            Logger::error($e->getMessage(), $e->getTrace());
-        }
-
-        // return plain-text password (realtime once) back
-        // for adding to Keycloak on next steps
-        $user->password = $newPassword;
-
-        return $user;
+        return $class->where('email', $user['email'])->first();
     }
-
 
 
     /**
@@ -109,7 +62,7 @@ class KeycloakUserController
      *
      * @param $user
      * @return bool
-    */
+     */
     public function createKeycloakUser($user): bool
     {
         $added_to_keycloak = false;
@@ -121,15 +74,13 @@ class KeycloakUserController
             $keycloakClient = Keycloak\Admin\KeycloakClient::factory($this->keycloakAdminController->getParams());
             $this->keycloakAdminController->setClient($keycloakClient);
 
-            if ( $this->keycloakAdminController->register( $this->prepareUserData($user) ) )
-            {
+            if ($this->keycloakAdminController->register($this->prepareUserData($user))) {
                 // app log User
                 AppLog::add('user', $user->id, 'keycloak');
 
                 return true;
             }
-        }
-        // something went wrong while
+        } // something went wrong while
         catch (\Exception $e) { // \Illuminate\Database\QueryException $e
             Logger::error($e->getMessage(), $e->getTrace());
         }
@@ -138,20 +89,19 @@ class KeycloakUserController
     }
 
 
-
     /**
      * Creating array from User's attributes
      *
      * @param $user
      * @return array
-    */
+     */
     protected function prepareUserData($user): array
     {
         $ret = [];
 
         $ret['email'] = $user->email;
         $ret['password'] = $user->password;
-        $ret['name'] = $user->name.' '.$user->surname;
+        $ret['name'] = $user->name . ' ' . $user->surname;
 
         return $ret;
     }
